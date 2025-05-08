755e1006a932f9ee59870cd48bd82b04815a2299
ratteirl <148093771+ratteirl@users.noreply.github.com>
2025-05-08 21:02:48 +0300

    savaş sistemi yapılacak

--- C_based_action_game.c
+++ C_based_action_game.c
@@ -1,40 +1,28 @@
-#include <stdio.h> 
+#include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
 #include <time.h>
 
 typedef struct {
-    char name[50];
+    char name[20];
     int attack_power;
     int health;
 
     void (*attack)(void* self, void* target);
     void (*defend)(void* self, int damage);
     void (*special_atack)(void* self, void* target);
-}Character;
+} Character;
 
 typedef struct {
     Character base;
     int rage; //savaşçının özel atağı
-}Warrior;
-
-Warrior* new_warrior(const char* name){
-    Warrior* w = malloc(sizeof(Warrior));
-    strcpy(w->base.name, name, sizeof(w->base.name));
-    w ->base.health =  100;
-    w ->base.attack_power = 20;
-    w ->base.attack = warrior_attack;
-    w ->base.defend = warrior_defend;
-    w ->base.special_atack = warrior_special_attack;
-    w->rage = 20;
-    return w;
-}
+} Warrior;
 
 void warrior_attack(void* self, void* target) {
     Warrior* warrior = (Warrior*)self;
     Character* enemy = (Character*)target;
     printf("%s SALDIRIYOR (%d hasar!)\n", warrior->base.name, enemy->name, warrior->base.attack);
-    enemy->defend(enemy, warrior->base.attack);
+    enemy->defend(enemy, warrior->base.attack_power);
 }
 
 void warrior_defend(void* self, int damage) {
@@ -43,8 +31,8 @@ void warrior_defend(void* self, int damage) {
     printf("%s HASAR ALINDI! (kalan can %d.)\n", warrior->base.name, damage, warrior->base.health);
 }
 
-void warrior special_atack(void* self, void* target) {
-    warrior* w = (warrior*)self;
+void special_atack(void* self, void* target) {
+    Warrior* w = (Warrior*)self;
     Character* defender = (Character*)target;
 
     if (w->rage >= 20){
@@ -56,28 +44,28 @@ void warrior special_atack(void* self, void* target) {
     }
 }
 
+Warrior* new_warrior(const char* name){
+    Warrior* w = malloc(sizeof(Warrior));
+    strcpy(w->base.name, name);
+    w ->base.health =  100;
+    w ->base.attack_power = 20;
+    w ->base.attack = warrior_attack;
+    w ->base.defend = warrior_defend;
+    w ->base.special_atack = special_atack;
+    w->rage = 20;
+    return w;
+}
+
 typedef struct {
     Character base;
     int supernova; //büyücünün özel atağı
-}Wizard;
-
-Wizard* new_wizard(const char* name){
-    Wizard* wiz = malloc(sizeof(Wizard));
-    strcpy(wiz->base.name, name, sizeof(wiz->base.name));
-    wiz ->base.health = 100;
-    wiz ->base.attack_power = 10;
-    wiz ->base.attack = wizard_attack;
-    wiz ->base.defend = wizard_defend;
-    wiz ->base.special_atack = wizard_special_attack;
-    wiz->supernova = 20;
-    return wiz;
-}
+} Wizard;
 
 void wizard_attack(void* self, void* target) {
     Wizard* wizard = (Wizard*)self;
     Character* enemy = (Character*)target;
     printf("%s SALDIRIYOR (%d hasar!)\n", wizard->base.name, enemy->name, wizard->base.attack);
-    enemy->defend(enemy, wizard->base.attack);
+    enemy->defend(enemy, wizard->base.attack_power);
 }
 
 void wizard_defend(void* self, int damage) {
@@ -97,6 +85,18 @@ void wizard_special_attack(void* self, void* target) {
     } else {
         printf("%s ÖZEL SALDIRI İÇİN YETERLİ MANAYA SAHİP DEĞİL!\n", wiz->base.name);
     }
+}
+
+Wizard* new_wizard(const char* name){
+    Wizard* wiz = malloc(sizeof(Wizard));
+    strcpy(wiz->base.name, name);
+    wiz ->base.health = 100;
+    wiz ->base.attack_power = 10;
+    wiz ->base.attack = wizard_attack;
+    wiz ->base.defend = wizard_defend;
+    wiz ->base.special_atack = wizard_special_attack;
+    wiz->supernova = 20;
+    return wiz;
 }
 
 int main(){
@@ -125,11 +125,15 @@ int main(){
     int enemy_type = rand() % 2;
     if (enemy_type == 0) 
         enemy = (Character*)new_warrior("Düşman Savaşçı");
-    else 
+    else{ 
         enemy = (Character*)new_wizard("Düşman Büyücü");
 
         printf("\n === SAVAŞ BAŞLIYOOOOOOR === \n\n");
-}}
+    }
+
+    getchar();
+    return 0;
+}
 
 
 
